70b340ef488f1da07f36f5eef347ec727259a974
muxanz <muxanzz@gmail.com>
2019-04-16 01:26:46 +0000

    [Feature] Function main created with success

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+/**
+ * main - Entry Point
+ * @ac: counter of arguments
+ * @av: arguments received in shell
+ * @env: environment
+ *
+ * Return: Always 0 (success)
+ */
+int main(int ac, char **av, char *env[])
+{
+	(void)ac;
+	_prompt(av, env);
+
+	return (0);
+}
